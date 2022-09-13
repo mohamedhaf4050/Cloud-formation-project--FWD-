b4e219e532922c8a531786985637650d7d0e8e58
Mohamed Ashraf <mohamed.hafez4050@gmail.com>
2022-09-13 23:28:54 +0200

    internetGateway&subnetsCreation

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
-aws cloudformation update-stack \
---stack-name $1
---stack-body file://$2 \
---parameters file://$3 \
---region-west-2
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
 $SHELL
